a51af02ac0ef78bbe1b63691d0e2b22ce33f0a18
doublnt <yinximail@gmail.com>
2017-10-24 16:00:03 +0800

    add ef web demo

---
+++ EF/EFWebDemo/DataBaseContext/BooksContext.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using EFWebDemo.Model;
+
+namespace EFWebDemo.DataBaseContext
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelBuilding(ModelBuilder modelBuiler)
+        {
+            modelBuiler.Entity<Book>()
+            .HasKey(p => p.Name);
+        }
+
+        public DbSet<Book> Books{get;set;} 
+    }
+}
---
+++ EF/EFWebDemo/Model/Book.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFWebDemo.Model
+{
+    public class Book
+    {
+        public string Name {get; set; }
+        public string Publisher { get; set; }
+        public double Price { get; set; }
+    }
+}
