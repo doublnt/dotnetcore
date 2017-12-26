87cc65a273d9b97d010ec322e06c506ecffe932a
robert <robert@cnblogs.com>
2017-12-26 18:20:11 +0800

    Add Multiple task test

--- MultipleTaskDemo/Program.cs
+++ MultipleTaskDemo/Program.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Diagnostics;
+using System.Linq;
 
 namespace MultipleTaskDemo
 {
     class Program
     {
+        public static IList<int> testList = new List<int>{3,4};
         static void Main(string[] args)
         {
-            bool a =false;
+            bool a = false;
             bool b = false;
             bool c = false;
             Stopwatch watch = new Stopwatch();//测量运行时间
@@ -18,6 +21,7 @@ namespace MultipleTaskDemo
             Thread threadTest1 = new Thread(() =>
              {
                  Thread.Sleep(2000);
+                 AddItem(1);
                  Console.WriteLine("线程1结束消耗时间：{0}", watch.ElapsedMilliseconds);
                  a = true;//如果执行则返回true
              });
@@ -26,6 +30,7 @@ namespace MultipleTaskDemo
             Thread threadTest2 = new Thread(() =>
            {
                Thread.Sleep(2000);
+               AddItem(1);
                Console.WriteLine("线程2结束消耗时间：{0}", watch.ElapsedMilliseconds);
                b = true;//如果执行则返回true
            });
@@ -34,6 +39,7 @@ namespace MultipleTaskDemo
             Thread threadTest3 = new Thread(() =>
            {
                Thread.Sleep(2000);
+               AddItem(2);
                Console.WriteLine("线程3结束消耗时间：{0}", watch.ElapsedMilliseconds);
                c = true;//如果执行则返回true
            });
@@ -49,13 +55,17 @@ namespace MultipleTaskDemo
             if (a == true && b == true && c == true)//当三个子线程全部执行完毕，则执行
             {
                 Console.WriteLine("监控结束消耗时间：{0}", watch.ElapsedMilliseconds);
+                Console.WriteLine(testList.Count);
                 Console.Read();
-
             }
         }
 
-        static void AddItem(){
-
+        static void AddItem(int a)
+        {
+            if (testList.IndexOf(a) < 0)
+            {
+                testList.Add(a);
+            }
         }
     }
 }
