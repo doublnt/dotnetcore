b4d71a2041f556252c2ec63c7b46ad94ead01814
doublnt <yinximail@gmail.com>
2017-12-26 17:46:33 +0800

    Add MultipleTask Demo

---
+++ MultipleTaskDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace MultipleTaskDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a =false;
+            bool b = false;
+            bool c = false;
+            Stopwatch watch = new Stopwatch();//测量运行时间
+            watch.Start();//开始计时
+
+            //线程1
+            Thread threadTest1 = new Thread(() =>
+             {
+                 Thread.Sleep(2000);
+                 Console.WriteLine("线程1结束消耗时间：{0}", watch.ElapsedMilliseconds);
+                 a = true;//如果执行则返回true
+             });
+
+            //线程2
+            Thread threadTest2 = new Thread(() =>
+           {
+               Thread.Sleep(2000);
+               Console.WriteLine("线程2结束消耗时间：{0}", watch.ElapsedMilliseconds);
+               b = true;//如果执行则返回true
+           });
+
+            //线程3
+            Thread threadTest3 = new Thread(() =>
+           {
+               Thread.Sleep(2000);
+               Console.WriteLine("线程3结束消耗时间：{0}", watch.ElapsedMilliseconds);
+               c = true;//如果执行则返回true
+           });
+
+            threadTest1.Start();
+            threadTest2.Start();
+            threadTest3.Start();
+
+            threadTest2.Join();//等待其它线程执行结束
+            threadTest1.Join();
+            threadTest3.Join();
+
+            if (a == true && b == true && c == true)//当三个子线程全部执行完毕，则执行
+            {
+                Console.WriteLine("监控结束消耗时间：{0}", watch.ElapsedMilliseconds);
+                Console.Read();
+
+            }
+        }
+
+        static void AddItem(){
+
+        }
+    }
+}
