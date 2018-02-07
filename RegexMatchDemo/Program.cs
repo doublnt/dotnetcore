27cdc9de6b47ff6350ae62068132fc21fa10a276
robert <robert@cnblogs.com>
2018-02-07 18:18:07 +0800

    Update the Demo

--- RegexMatchDemo/Program.cs
+++ RegexMatchDemo/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
-using System.Text.RegularExpressions;
 using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace RegexMatchDemo {
-    class Program {
-        static void Main (string[] args) {
+namespace RegexMatchDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             var text = @"http://q.cnblogs.com/test
 q.cnblogs.com/
 cnblogs.com/
@@ -12,16 +15,16 @@ www.baidu.com/
 www.sina.com/
 www.google.com/";
 
-            var regex = new Regex (@"(?:[ａ-ｚa-z0-9]{1-50}[.．])?([ａ-ｚa-z0-9]{1,20}[.．][ａ-ｚa-z]{2,4}(?:[.．][ａ-ｚa-z]{2})?)/");
-            var matches = regex.Matches (text);
-            var domains = matches.Select (m => m.Groups[1].Value);
+            var regex = new Regex(@"(?:[ａ-ｚa-z0-9]{1-50}[.．])?([ａ-ｚa-z0-9]{1,20}[.．][ａ-ｚa-z]{2,4}(?:[.．][ａ-ｚa-z]{2})?)/");
+            var matches = regex.Matches(text);
+            var domains = matches.Select(m => m.Groups[1].Value);
 
-            var result = domains.GroupBy (x => x)
-                .Select (x => new { Domain = x.Key, Count = x.Count () })
-                .OrderByDescending (x => x.Count)
-                .FirstOrDefault ();
+            var result = domains.GroupBy(x => x)
+                .Select(x => new { Domain = x.Key, Count = x.Count() })
+                .OrderByDescending(x => x.Count)
+                .FirstOrDefault();
 
-            Console.WriteLine ($"{result.Domain}, {result.Count}");
+            Console.WriteLine($"{result.Domain}, {result.Count}");
         }
     }
 }
