0dc97fdbe3e7fd607f6c60485c71a43fc815972f
doublnt <yinximail@gmail.com>
2018-02-07 17:48:59 +0800

    Add RegexDemo

---
+++ RegexMatchDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace RegexMatchDemo {
+    class Program {
+        static void Main (string[] args) {
+            var text = @"http://q.cnblogs.com/test
+q.cnblogs.com/
+cnblogs.com/
+www.baidu.com/
+www.sina.com/
+www.google.com/";
+
+            var regex = new Regex (@"(?:[ａ-ｚa-z0-9]{1-50}[.．])?([ａ-ｚa-z0-9]{1,20}[.．][ａ-ｚa-z]{2,4}(?:[.．][ａ-ｚa-z]{2})?)/");
+            var matches = regex.Matches (text);
+            var domains = matches.Select (m => m.Groups[1].Value);
+
+            var result = domains.GroupBy (x => x)
+                .Select (x => new { Domain = x.Key, Count = x.Count () })
+                .OrderByDescending (x => x.Count)
+                .FirstOrDefault ();
+
+            Console.WriteLine ($"{result.Domain}, {result.Count}");
+        }
+    }
+}
