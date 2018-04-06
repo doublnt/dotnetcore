18f2f30fb0fb701e135efa76231f9aab89dcc45d
doublnt <yinximail@gmail.com>
2018-04-06 19:32:10 +0800

    Add EventDemo

---
+++ EventDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EventDemo {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
