857d65b9cbe19ac3523c6a7c6ecec9954d614ffa
doublnt <yinximail@gmail.com>
2018-04-09 16:29:56 +0800

    Add Custom Event Demo

--- Event/CustomEvent/Program.cs
+++ Event/CustomEvent/Program.cs
@@ -1,41 +1,64 @@
 ﻿using System;
 
 namespace CustomEvent {
-    // public class CustomEventArgs : EventArgs {
-    //     public CustomEventArgs (string s) {
-    //         message = s;
-    //     }
 
-    //     private string message;
+    public delegate void CustomEventDelegate (object sender, CustomEventArgs e);
+    
+    public class CustomEventArgs : EventArgs {
 
-    //     public string Mesage {
-    //         get {
-    //             return message
-    //         }
-    //         set {
-    //             message = value;
-    //         }
-    //     }
-    // }
+        public CustomEventArgs (string s) {
+            message = s;
+        }
 
-    // class Publisher {
-    //     public event EventHandler<CustomEventArgs> RaiseCustomEvent;
+        private string message;
 
-    //     public void DoSomething(){
-    //         OnRaiseCustomEvent()
-    //     }
+        public string Message {
+            get {
+                return message;
+            }
+            set {
+                message = value;
+            }
+        }
+    }
+    class Publisher {
+        public event CustomEventDelegate RaiseCustomEvent;
 
-    //     protected virtual void OnRaiseCustomEvent(CustomEventArgs e){
-    //         EventHandler<CustomEventArgs> handler = RaiseCustomEvent;
-    //         if (handler != null){
-    //             e.Mesage += String.Format("at {0}",DateTime.Now.ToString)
-    //         }
-    //     }
-    // }
+        public void DoSomething () {
+            OnRaiseCustomEvent (new CustomEventArgs ("Did something"));
+        }
+
+        protected virtual void OnRaiseCustomEvent (CustomEventArgs e) {
+            CustomEventDelegate handler = RaiseCustomEvent;
+
+            if (handler != null) {
+                e.Message += String.Format ("at {0}", DateTime.Now.ToString ());
+
+                handler (this, e);
+            }
+        }
+    }
+
+    class Subscriber {
+        private string id;
+
+        public Subscriber (string ID, Publisher pub) {
+            id = ID;
+            pub.RaiseCustomEvent += HandleCustomEvent;
+        }
+
+        void HandleCustomEvent (object sender, CustomEventArgs e) {
+            Console.WriteLine (id + " received this message: {0}", e.Message);
+        }
+    }
 
     class Program {
         static void Main (string[] args) {
-            Console.WriteLine ("Hello World!");
+            Publisher pub = new Publisher ();
+            Subscriber sub1 = new Subscriber ("sub1", pub);
+            Subscriber sub2 = new Subscriber ("sub2", pub);
+
+            pub.DoSomething ();
         }
     }
 }
