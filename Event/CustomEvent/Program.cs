081fddf1ba2961b0d4126afd38109e87b2fa1f3d
doublnt <yinximail@gmail.com>
2018-04-09 14:55:05 +0800

    Update

---
+++ Event/CustomEvent/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CustomEvent {
+    // public class CustomEventArgs : EventArgs {
+    //     public CustomEventArgs (string s) {
+    //         message = s;
+    //     }
+
+    //     private string message;
+
+    //     public string Mesage {
+    //         get {
+    //             return message
+    //         }
+    //         set {
+    //             message = value;
+    //         }
+    //     }
+    // }
+
+    // class Publisher {
+    //     public event EventHandler<CustomEventArgs> RaiseCustomEvent;
+
+    //     public void DoSomething(){
+    //         OnRaiseCustomEvent()
+    //     }
+
+    //     protected virtual void OnRaiseCustomEvent(CustomEventArgs e){
+    //         EventHandler<CustomEventArgs> handler = RaiseCustomEvent;
+    //         if (handler != null){
+    //             e.Mesage += String.Format("at {0}",DateTime.Now.ToString)
+    //         }
+    //     }
+    // }
+
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
