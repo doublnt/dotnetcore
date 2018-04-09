586be1ae45c4344d7b863bcb25b952ee2d1cfdf3
doublnt <yinximail@gmail.com>
2018-04-09 11:43:51 +0800

    Add multiple delegate demo

--- EventDemo/Program.cs
+++ EventDemo/Program.cs
@@ -4,13 +4,30 @@ using System.Threading.Tasks;
 namespace EventDemo {
     public class Program {
         public delegate string SendMessage (string sender, string receiver);
-        public static void Main (string[] args) { 
-            SendMessage mailSend = new SendMessage(new Program().MailSendMessage);
+        public static void Main (string[] args) {
+            SendMessage mailSend = new SendMessage (new Program ().MailSendMessage);
+            Console.WriteLine (mailSend ("Robert", "Mike"));
 
-            Console.WriteLine(mailSend("Robert","Mike"));
+            Action delActions = OneVoke;
+            delActions += TwoVoke;
+
+            try {
+                delActions.Invoke ();
+            } catch (Exception ex) {
+                Console.WriteLine ("I Caught you,", ex);
+            }
         }
         public string MailSendMessage (string sender, string receiver) {
             return "From:" + sender + "\nTo:" + receiver;
+        }
+
+        static void OneVoke () {
+            Console.WriteLine ("One invoked!");
+            throw new Exception ("Error in one invoke");
+        }
+
+        static void TwoVoke () {
+            Console.WriteLine ("Two invoked!");
         }
     }
 }
