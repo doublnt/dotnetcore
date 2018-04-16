23e28443bc457a5994a2ce39ecc00002c959b594
doublnt <yinximail@gmail.com>
2018-04-16 18:12:25 +0800

    Add test

--- RabbitMQ/Send/Send.cs
+++ RabbitMQ/Send/Send.cs
@@ -2,32 +2,37 @@
 using System.Text;
 using RabbitMQ.Client;
 
-namespace Send {
-    class Program {
-        static void Main (string[] args) {
-            var factory = new ConnectionFactory () { HostName = "localhost" };
-            using (var connection = factory.CreateConnection ()) {
-                using (var channel = connection.CreateModel ()) {
-                    channel.QueueDeclare (
+namespace Send
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
                         queue: "hello",
-                        durable : false,
-                        exclusive : false,
-                        autoDelete : false,
-                        arguments : null
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
                     );
                     string message = "Hello, World";
 
-                    var body = Encoding.UTF8.GetBytes (message);
+                    var body = Encoding.UTF8.GetBytes(message);
 
-                    channel.BasicPublish (
+                    channel.BasicPublish(
                         exchange: "",
                         routingKey: "hello",
-                        basicProperties : null,
-                        body : body
+                        basicProperties: null,
+                        body: body
                     );
 
-                    Console.WriteLine ("Press [enter] to exit.");
-                    Console.ReadLine ();
+                    Console.WriteLine("Press [enter] to exit.");
+                    Console.ReadLine();
                 }
             }
         }
