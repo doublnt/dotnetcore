1156990de51afe079cde05191cc2a709e695a8d9
doublnt <yinximail@gmail.com>
2018-04-16 12:20:29 +0800

    Add braces

--- RabbitMQ/Send/Send.cs
+++ RabbitMQ/Send/Send.cs
@@ -6,28 +6,29 @@ namespace Send {
     class Program {
         static void Main (string[] args) {
             var factory = new ConnectionFactory () { HostName = "localhost" };
-            using (var connection = factory.CreateConnection ())
-            using (var channel = connection.CreateModel ()) {
-                channel.QueueDeclare (
-                    queue: "hello",
-                    durable : false,
-                    exclusive : false,
-                    autoDelete : false,
-                    arguments : null
-                );
-                string message = "Hello, World";
+            using (var connection = factory.CreateConnection ()) {
+                using (var channel = connection.CreateModel ()) {
+                    channel.QueueDeclare (
+                        queue: "hello",
+                        durable : false,
+                        exclusive : false,
+                        autoDelete : false,
+                        arguments : null
+                    );
+                    string message = "Hello, World";
 
-                var body = Encoding.UTF8.GetBytes (message);
+                    var body = Encoding.UTF8.GetBytes (message);
 
-                channel.BasicPublish (
-                    exchange: "",
-                    routingKey: "hello",
-                    basicProperties : null,
-                    body : body
-                );
+                    channel.BasicPublish (
+                        exchange: "",
+                        routingKey: "hello",
+                        basicProperties : null,
+                        body : body
+                    );
 
-                Console.WriteLine ("Press [enter] to exit.");
-                Console.ReadLine ();
+                    Console.WriteLine ("Press [enter] to exit.");
+                    Console.ReadLine ();
+                }
             }
         }
     }
