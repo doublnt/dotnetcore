df87d3bd18434f9e0f9931076f1dd0b813a6cfe1
doublnt <yinximail@gmail.com>
2019-01-21 14:53:35 +0800

    Add the AbstractDemo Code

---
+++ AbstractDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bird a = new ExplictBird("DiDi");
+            
+            a.PrintBird();
+        }
+    }
+
+    abstract class Bird
+    {
+        private string BirdName;
+
+        public abstract void PrintBird();
+
+        public void PrintBirdSon()
+        {
+            Console.WriteLine("Hello, World!" + BirdName);
+        }
+        public string GetName(){
+            return BirdName;
+        }
+        
+        public void SetName(string name){
+            BirdName = name;
+        }
+    }
+
+    class ExplictBird : Bird
+    {
+        public ExplictBird(string name){
+            base.SetName(name);
+        }
+        public override void PrintBird()
+        {
+            Console.WriteLine("Hello, Bird" + base.GetName());
+        }
+    }
+}
