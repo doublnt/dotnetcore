02e383fb65d975cac4afdbf4d053e5ed9665497d
doublnt <yinximail@gmail.com>
2019-02-18 20:01:17 +0800

    Create Project

---
+++ dotnet-core-version/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_core_version
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
