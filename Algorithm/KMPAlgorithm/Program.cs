c7dfa7d9f620d95b9c98da3d5e87924b033c9b11
doublnt <yinximail@gmail.com>
2019-02-27 17:12:42 +0800

    KMP Algorithm

---
+++ Algorithm/KMPAlgorithm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KMPAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = "Code/111";
+            var content = "Alibaba/222/Code/222/Code/111";
+
+            new KMP().KMPSearch(pattern, content);
+        }
+    }
+}
