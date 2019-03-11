a790925f467aa196efc5a94c528e4567003e6929
doublnt <yinximail@gmail.com>
2019-03-11 10:02:27 +0800

    Add aliyun test demo

--- Algorithm/LRUDEMO/Program.cs
+++ Algorithm/LRUDEMO/Program.cs
@@ -7,20 +7,22 @@ namespace LRUDemo
         static void Main(string[] args)
         {
             LRUCache<int, int> cache = new LRUCache<int, int>(5);
-            cache.AddToFirst(1,1);
-            cache.AddToFirst(2,2);
-            cache.AddToFirst(3,3);
-            cache.AddToFirst(4,4);
-            cache.AddToFirst(5,5);
+            cache.AddToFirst(1, 1);
+            cache.AddToFirst(2, 2);
+            cache.AddToFirst(3, 3);
+            cache.AddToFirst(4, 4);
+            cache.AddToFirst(5, 5);
 
             cache.DisplayList();
 
-            cache.AddToFirst(3,3);
+            cache.AddToFirst(3, 3);
             cache.DisplayList();
 
-            cache.AddToFirst(6,6);
-            cache.AddToFirst(7,7);
+            cache.AddToFirst(6, 6);
+            cache.AddToFirst(7, 7);
             cache.DisplayList();
+
+            Console.WriteLine(null == 1);
         }
     }
 }
---
+++ aliyun-core-demo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Profile;
+using Aliyun.Acs.Ecs.Model.V20140526;
+
+namespace Aliyun.Core.Demo
+{
+    class Program
+    {
+        private static string regionId = "cn-hangzhou";
+        private static string accessKey;
+        private static string accessKeySecret;
+
+        private static IClientProfile profile;
+        private static DefaultAcsClient client;
+
+        static void Main(string[] args)
+        {
+            accessKey = Environment.GetEnvironmentVariable("ACCESS_KEY_ID") ?? "AccessKeyId";
+            accessKeySecret = Environment.GetEnvironmentVariable("ACCESS_KEY_SECRET") ?? "AccessKeySecret";
+
+            profile = DefaultProfile.GetProfile(regionId, accessKey, accessKeySecret);
+            client = new DefaultAcsClient(profile);
+
+            DescribeRegionsRequest request = new DescribeRegionsRequest();
+            DescribeRegionsResponse response = client.GetAcsResponse(request);
+
+            // Console.WriteLine(System.Text.Encoding.UTF8.GetString(response.HttpResponse.Content));
+            foreach (KeyValuePair<string, string> item in request.Headers)
+            {
+                Console.WriteLine("Key: " + item.Key + "   Value: " + item.Value);
+            }
+        }
+    }
+}
