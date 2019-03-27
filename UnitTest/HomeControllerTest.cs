91ccc5f09733c97651f9e3ffbd5761c7479a2bea
doublnt <yinximail@gmail.com>
2019-03-27 21:26:03 +0800

    regex test

--- UnitTest/HomeControllerTest.cs
+++ UnitTest/HomeControllerTest.cs
@@ -3,30 +3,39 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace TestingControllersSample.Tests.IntegrationTests
 {
-    public class HomeControllerTests : IClassFixture<TestFixture<Startup>>
+    // public class HomeControllerTests : IClassFixture<TestFixture<Startup>>
+    public class HomeControllerTests
     {
         private readonly HttpClient _client;
 
-        public HomeControllerTests(TestFixture<MiddlewareDemo.Startup> fixture)
-        {
-            _client = fixture.Client;
-        }
+        private static string LogTemplate = "[{time}] {channel} {level}: '{method} {uri} HTTP/{version}' {code} {cost}ms {MachineName} {ProcessId} {ThreadId}";
+        private static string regexPattern = @"{(.*?)}";
+        private static IDictionary<string, string> LogMessageValueMap = new Dictionary<string, string>();
+
+        // public HomeControllerTests(TestFixture<MiddlewareDemo.Startup> fixture)
+        // {
+        //     _client = fixture.Client;
+        // }
 
         [Fact]
-        public async Task WelcomeTest()
+        public void WelcomeTest()
         {
-            // Act
-            var response = await _client.GetAsync("/test");
+            Regex re = new Regex(regexPattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            var matches = re.Matches(LogTemplate);
 
-            // Assert
-            response.EnsureSuccessStatusCode();
-            var responseString = await response.Content.ReadAsStringAsync();
-
+            if (0 < matches.Count)
+            {
+                foreach (Match item in matches)
+                {
+                    LogMessageValueMap.Add
+                }
+            }
         }
     }
 }
