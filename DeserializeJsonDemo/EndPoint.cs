2fd029088f9aae738fb493d2e8713c05f42b9e6a
YinXi <yinximail@gmail.com>
2019-04-04 17:16:55 +0800

    json read

---
+++ DeserializeJsonDemo/EndPoint.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace DeserializeJsonDemo
+{
+    public class EndPoint
+    {
+        public string RegionId { get; set; }
+        public string Domain { get; set; }
+    }
+}
