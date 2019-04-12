da5fd8bffad006f19ff14526fc341e5df99fff19
YinXi <yinximail@gmail.com>
2019-04-12 17:08:43 +0800

    Push event webhook test

--- aliyun-core-demo/Program.cs
+++ aliyun-core-demo/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using Aliyun.Acs.Core;
 using Aliyun.Acs.Core.Exceptions;
 using Aliyun.Acs.Core.Http;
 using Aliyun.Acs.Core.Profile;
-using Aliyun.Acs.Mts.Model.V20140618;
+using Aliyun.Acs.industry_brain.Model.V20180712;
+using Newtonsoft.Json;
 
 namespace CommonRequestDemo {
     class Program {
@@ -12,15 +14,35 @@ namespace CommonRequestDemo {
             var accessKey = Environment.GetEnvironmentVariable ("ACCESS_KEY_ID") ?? "AccessKeyId";
             var accessKeySecret = Environment.GetEnvironmentVariable ("ACCESS_KEY_SECRET") ?? "AccessKeySecret";
 
+            DefaultProfile.AddEndpoint ("cn-hangzhou", "cn-hangzhou", "industry_brain", "industrial-brain.cn-hangzhou.aliyuncs.com");
             IClientProfile profile = DefaultProfile.GetProfile ("cn-hangzhou", accessKey, accessKeySecret);
             DefaultAcsClient client = new DefaultAcsClient (profile);
 
-            var request = new SubmitJobsRequest();
-            request.Input="{\"Bucket\":\"sgfa-sdf\",\"Location\":\"oss-cn-hangzhou\",\"Object\":\"test.flv\"}";
-            request.OutputBucket = "bucket-output-template";
-            request.OutputLocation="oss-cn-hangzhou";
-            request.PipelineId = "testpinpsfsd";
-            request.Outputs = "[{\"OutputObject\":\"test-output.flv\",\"TemplateId\":\"testtempalte\",\"UserData\":\"testid-001\"}]";
+            // var request = new SubmitJobsRequest();
+            // request.Input="{\"Bucket\":\"sgfa-sdf\",\"Location\":\"oss-cn-hangzhou\",\"Object\":\"test.flv\"}";
+            // request.OutputBucket = "bucket-output-template";
+            // request.OutputLocation="oss-cn-hangzhou";
+            // request.PipelineId = "testpinpsfsd";
+            // request.Outputs = "[{\"OutputObject\":\"test-output.flv\",\"TemplateId\":\"testtempalte\",\"UserData\":\"testid-001\"}]";
+
+            var request = new GetIndustryInfoChildrenListRequest();
+
+            // var items = new List<dynamic> ();
+
+            // dynamic obj = new ExpandoObject ();
+
+            // obj.key = "LightAdjustLevel";
+            // obj.value = 1;
+            // items.Add (obj);
+
+            // request.ProductKey = "a1LsCUpgf5n";
+            // request.DeviceName = "yinxi-test";
+
+            // var jsonItem = JsonConvert.SerializeObject(items);
+            // Console.WriteLine(jsonItem);
+
+            // jsonItem = "{LightAdjustLevel:1}";
+            // request.Items = jsonItem;
 
             try {
                 var response = client.GetAcsResponse (request);
