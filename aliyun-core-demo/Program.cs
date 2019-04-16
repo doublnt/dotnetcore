39879b593a7b1691fe0ba075a42f5a93286cfcce
YinXi <yinximail@gmail.com>
2019-04-16 19:21:38 +0800

    WebhookPush test

--- aliyun-core-demo/Program.cs
+++ aliyun-core-demo/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Dynamic;
+
 using Aliyun.Acs.Core;
 using Aliyun.Acs.Core.Exceptions;
 using Aliyun.Acs.Core.Http;
 using Aliyun.Acs.Core.Profile;
+using Aliyun.Acs.Domain_intl.Model.V20171218;
 using Aliyun.Acs.industry_brain.Model.V20180712;
 using Aliyun.Acs.ImageSearch.Model.V20190325;
+using Aliyun.Acs.Iot.Model.V20180120;
+using Aliyun.Acs.vod.Model.V20170321;
+
 using Newtonsoft.Json;
 
 namespace CommonRequestDemo
@@ -19,9 +24,12 @@ namespace CommonRequestDemo
             var accessKeySecret = Environment.GetEnvironmentVariable("ACCESS_KEY_SECRET") ?? "AccessKeySecret";
 
             //DefaultProfile.AddEndpoint ("cn-hangzhou", "cn-hangzhou", "industry_brain", "industrial-brain.cn-hangzhou.aliyuncs.com");
-            IClientProfile profile = DefaultProfile.GetProfile("cn-shanghai", accessKey, accessKeySecret);
-            DefaultAcsClient client = new DefaultAcsClient(profile);
+            //IClientProfile profile = DefaultProfile.GetProfile("cn-shanghai", accessKey, accessKeySecret);
+            //DefaultAcsClient client = new DefaultAcsClient(profile);
 
+            var client = InitVodClient(accessKey, accessKeySecret);
+
+            #region commend code
             // var request = new SubmitJobsRequest();
             // request.Input="{\"Bucket\":\"sgfa-sdf\",\"Location\":\"oss-cn-hangzhou\",\"Object\":\"test.flv\"}";
             // request.OutputBucket = "bucket-output-template";
@@ -29,13 +37,13 @@ namespace CommonRequestDemo
             // request.PipelineId = "testpinpsfsd";
             // request.Outputs = "[{\"OutputObject\":\"test-output.flv\",\"TemplateId\":\"testtempalte\",\"UserData\":\"testid-001\"}]";
 
-            AddImageRequest request = new AddImageRequest();
-            request.InstanceName = "testinstance";
-            request.ProductId = "test";
-            request.PicName = "test";
-            byte[] img = System.IO.File.ReadAllBytes("D:/test.jpg");
-            string pic = Convert.ToBase64String(img);
-            request.PicContent = pic;
+            //AddImageRequest request = new AddImageRequest();
+            //request.InstanceName = "testinstance";
+            //request.ProductId = "test";
+            //request.PicName = "test";
+            //byte[] img = System.IO.File.ReadAllBytes("D:/test.jpg");
+            //string pic = Convert.ToBase64String(img);
+            //request.PicContent = pic;
 
             // var items = new List<dynamic> ();
 
@@ -54,9 +62,29 @@ namespace CommonRequestDemo
             // jsonItem = "{LightAdjustLevel:1}";
             // request.Items = jsonItem;
 
+            //var request = new QueryDevicePropertyStatusRequest();
+            //request.ProductKey = "a1xplY8eBQT";
+            //request.DeviceName = "SzAfkec0rv0RgfdnicbK";
+
+            //var request = new SetDevicePropertyRequest();
+            //request.ProductKey = "a1xplY8eBQT";
+            //request.DeviceName = "SzAfkec0rv0RgfdnicbK";
+
+            //request.Items = "{\"AlarmText\":\"E7BB93E69D9FE5BE85E69CBAE4B8AD20202020202020202020202020202020202020202020202020202020202030303830202020202020202020202020202020207C4F50454E2054484520444F4F52205448454E20434C4F5345207C\"}";
+
+            #endregion
+
+            var request = new CreateUploadVideoRequest();
+            request.FileName = "03552d1d9235583a5e20ce3b677ea176.mp4";
+            request.Title = null;
+
             try
             {
                 var response = client.GetAcsResponse(request);
+                Console.WriteLine(response.UploadAddress);
+                Console.WriteLine(response.UploadAddress);
+                Console.WriteLine(response.RequestId);
+                Console.WriteLine(response.VideoId);
                 Console.WriteLine(System.Text.Encoding.Default.GetString(response.HttpResponse.Content));
             }
             catch (ServerException e)
@@ -67,6 +95,15 @@ namespace CommonRequestDemo
             {
                 Console.WriteLine(e);
             }
+        }
+
+        public static DefaultAcsClient InitVodClient(string accessKeyId, string accessKeySecret)
+        {
+            // 点播服务接入区域
+            string regionId = "cn-shanghai";
+            IClientProfile profile = DefaultProfile.GetProfile(regionId, accessKeyId, accessKeySecret);
+            // DefaultProfile.AddEndpoint(regionId, regionId, "vod", "vod." + regionId + ".aliyuncs.com");
+            return new DefaultAcsClient(profile);
         }
     }
 }
--- webhook_post_sample/Controllers/ValuesController.cs
+++ webhook_post_sample/Controllers/ValuesController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace webhook_post_sample.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
@@ -17,30 +18,28 @@ namespace webhook_post_sample.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
+        // // GET api/values/5
+        // [HttpGet("{id}")]
+        // public ActionResult<string> Get(int id)
+        // {
+        //     return "value";
+        // }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post()
         {
-
+            Console.WriteLine("test");
         }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        // [HttpPut("{id}")]
+        // public void Put(int id, [FromBody] string value)
+        // { }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        // // DELETE api/values/5
+        // [HttpDelete("{id}")]
+        // public void Delete(int id)
+        // { }
     }
 }
