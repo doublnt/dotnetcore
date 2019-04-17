6f74657612661be08a7151c62c1415a14516b7ba
YinXi <yinximail@gmail.com>
2019-04-17 18:50:03 +0800

    WebHook download specific file

--- webhook_post_sample/Controllers/ValuesController.cs
+++ webhook_post_sample/Controllers/ValuesController.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
+using Newtonsoft.Json;
+
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using webhook_post_sample.Model;
 
 namespace webhook_post_sample.Controllers
 {
@@ -13,6 +20,8 @@ namespace webhook_post_sample.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private List<string> DocFileList = new List<string>();
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
@@ -31,11 +40,76 @@ namespace webhook_post_sample.Controllers
         [HttpPost("/payload")]
         public async Task<string> Post()
         {
-            using (StreamReader streamReader = new StreamReader(Request.Body, Encoding.UTF8))
+            try
             {
-                var value = await streamReader.ReadToEndAsync();
-                Console.WriteLine(value);
-                return value;
+                using (StreamReader streamReader = new StreamReader(Request.Body, Encoding.UTF8))
+                {
+                    await DeserializeJsonFromStream(streamReader);
+
+                    return await streamReader.ReadToEndAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private async Task DeserializeJsonFromStream(StreamReader streamReader)
+        {
+            var json = await streamReader.ReadToEndAsync();
+            var job = JObject.Parse(json);
+
+            IList<JToken> results = job["commits"].Children().ToList();
+            IList<WebhookCommit> webhookCommitsList = new List<WebhookCommit>();
+
+            foreach (var result in results)
+            {
+                var commits = result.ToObject<WebhookCommit>();
+                webhookCommitsList.Add(commits);
+            }
+
+            AddChangeFileToList(webhookCommitsList);
+
+            DownLoadFileToLocal(DocFileList);
+        }
+
+        private void AddChangeFileToList(IList<WebhookCommit> commits)
+        {
+            foreach (var item in commits)
+            {
+                AddDownloadFileToList(item.Added);
+                AddDownloadFileToList(item.Modified);
+            }
+        }
+
+        private void AddDownloadFileToList(IList<string> docList)
+        {
+            foreach (var item in docList)
+            {
+                if (DocFileList.Contains(item)) continue;
+
+                DocFileList.Add(item);
+            }
+        }
+
+        private async void DownLoadFileToLocal(IList<string> docList)
+        {
+            string baseUrl = "https://raw.githubusercontent.com/doublnt/dotnetcore/master/";
+            string homeDirectory = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+
+            var targetDirectory = homeDirectory;
+            Directory.CreateDirectory(targetDirectory + "/Docs/");
+
+            using (WebClient webClient = new WebClient())
+            {
+                foreach (var file in docList)
+                {
+                    var fileName = new Uri(baseUrl + file);
+
+                    await webClient.DownloadFileTaskAsync(fileName, targetDirectory + "/" + file);
+                }
             }
         }
 
---
+++ webhook_post_sample/Model/Webhook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace webhook_post_sample.Model
+{
+    class WebhookCommit
+    {
+        public string ID { get; set; }
+        public string Tree_ID { get; set; }
+        public bool Distinct { get; set; }
+        public string Message { get; set; }
+        public string TimeStamp { get; set; }
+        public string Url { get; set; }
+        public AuthorInfo Author { get; set; }
+        public AuthorInfo Committer { get; set; }
+        public List<string> Added { get; set; }
+        public List<string> Removed { get; set; }
+        public List<string> Modified { get; set; }
+    }
+
+    internal class AuthorInfo
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+    }
+}
