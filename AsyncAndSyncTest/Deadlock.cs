f6e913e2dfacf35665ff14617df49d8525c37164
YinXi <yinximail@gmail.com>
2019-05-09 17:34:41 +0800

    Update app domain code

---
+++ AsyncAndSyncTest/Deadlock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAndSyncTest
+{
+    public class Deadlock
+    {
+        public async Task<string> GetResponseValue()
+        {
+            return await GetResponseAsync();
+        }
+
+        public async Task<string> GetResponseAsync()
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://baidu.com");
+
+            var response = await httpWebRequest.GetResponseAsync();
+            return response.ContentType;
+        }
+    }
+}
--- AsyncAndSyncTest/Program.cs
+++ AsyncAndSyncTest/Program.cs
@@ -2,12 +2,19 @@
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsyncAndSyncTest
 {
     class Program
     {
+        /// <summary>
+        /// C# 7.1 feature async Main Function
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -17,29 +24,39 @@ namespace AsyncAndSyncTest
 
             var value2 = await GetWebStringCountAsync();
             Console.WriteLine(value2);
+
+            //Deadlock code
+            //            var deaklock = new Deadlock();
+            //            await deaklock.GetResponseValue();
+            //
+            //            Console.WriteLine(await deaklock.GetResponseValue());
+
+            Marshalling();
         }
 
         static async Task<string> GetWebStringCountAsync()
         {
-//            Stopwatch watch = new Stopwatch();
-//            watch.Start();
-//            var request = (HttpWebRequest)WebRequest.Create("https://www.baidu.com");
-//            var response = (HttpWebResponse)await request.GetResponseAsync();
-//            watch.Stop();
-//            Console.WriteLine("The Async Current execute Time is(ms):" + watch.ElapsedMilliseconds);
-//
-//            return response.StatusCode.ToString();
-            
+            //            Stopwatch watch = new Stopwatch();
+            //            watch.Start();
+            //            var request = (HttpWebRequest)WebRequest.Create("https://www.baidu.com");
+            //            var response = (HttpWebResponse)await request.GetResponseAsync();
+            //            watch.Stop();
+            //            Console.WriteLine("The Async Current execute Time is(ms):" + watch.ElapsedMilliseconds);
+            //
+            //            return response.StatusCode.ToString();
+
             Stopwatch watch = new Stopwatch();
             watch.Start();
             var task = Task.Run(async () =>
             {
-                var request = (HttpWebRequest) WebRequest.Create("https://www.baidu.com");
-                var response = (HttpWebResponse) await request.GetResponseAsync();
+                var request = (HttpWebRequest)WebRequest.Create("https://www.baidu.com");
+                var response = (HttpWebResponse)await request.GetResponseAsync();
                 return response.StatusCode.ToString();
             });
             watch.Stop();
             Console.WriteLine("The Async Current execute Time is(ms):" + watch.ElapsedMilliseconds);
+            Console.WriteLine(System.Environment.CurrentManagedThreadId);
+
             return await task;
         }
 
@@ -51,8 +68,28 @@ namespace AsyncAndSyncTest
             var response = (HttpWebResponse)request.GetResponse();
             watch.Stop();
             Console.WriteLine("The Sync Current execute Time is(ms):" + watch.ElapsedMilliseconds);
+            Console.WriteLine(System.Environment.CurrentManagedThreadId);
 
             return response.StatusCode.ToString();
+        }
+
+        private static void Marshalling()
+        {
+            AppDomain adCallingDomainName = Thread.GetDomain();
+
+            String callingDomainName = adCallingDomainName.FriendlyName;
+            Console.WriteLine("Default AppDomain's friendly name={0}", callingDomainName);
+
+            String exeAssembly = Assembly.GetEntryAssembly().FullName;
+            Console.WriteLine("Main Assembly={0}", exeAssembly);
+
+            AppDomain ad2 = null;
+            Console.WriteLine("{0} DEMO#1", Environment.NewLine);
+
+            ad2 = AppDomain.CreateDomain("AD #2");
+            MarshalByRefObject mbrt = null;
+
+            AppDomain.Unload(ad2);
         }
     }
 }
