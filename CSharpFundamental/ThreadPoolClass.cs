d12c72b9014d2956386dac036a4637713db21137
YinXi <yinximail@gmail.com>
2019-05-10 13:44:22 +0800

    Cancel Token test

--- CSharpFundamental/ThreadPoolClass.cs
+++ CSharpFundamental/ThreadPoolClass.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.Remoting.Messaging;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace CSharpFundamental
 {
@@ -37,10 +39,11 @@ namespace CSharpFundamental
 
 
             //            SuppressFlowTest();
-            //
-            ThreadCancelTest();
-            CancelRegister();
-            LinkCancelTokenSource();
+            //            ThreadCancelTest();
+            //            CancelRegister();
+            //            LinkCancelTokenSource();
+
+            DoTaskSum();
         }
 
         private void SuppressFlowTest()
@@ -55,7 +58,12 @@ namespace CSharpFundamental
 
             ExecutionContext.RestoreFlow();
 
-            Console.ReadLine();
+            //            Console.ReadLine();
+            //Use task to do the same thing
+
+            new Task(() => Console.WriteLine("Hello,World")).Start();
+            Task.Run(() => Console.WriteLine("Hello,World Second version."));
+            Task.Run(() => Console.WriteLine("Version three just for consturctor"), CancellationToken.None);
         }
 
         private void ThreadCancelTest()
@@ -115,8 +123,88 @@ namespace CSharpFundamental
 
             cts2.Cancel();
 
-            Console.WriteLine("Cts1 canceled={0}, Cts2 canceled={1} linkedCts canceled={2}", 
+            Console.WriteLine("Cts1 canceled={0}, Cts2 canceled={1} linkedCts canceled={2}",
                 cts1.IsCancellationRequested, cts2.IsCancellationRequested, linkedCts.IsCancellationRequested);
+        }
+
+        private Int32 Sum(Int32 n)
+        {
+            //Calculate the n + n-1 + ...+1
+            Int32 sum = 0;
+
+            for (; n > 0; n--)
+            {
+                checked
+                {
+                    sum += n;
+                }
+            }
+
+            return sum;
+        }
+
+        private Int32 SumWithCancelToken(CancellationToken ct, Int32 n)
+        {
+            Int32 sum = 0;
+
+            for (; n > 0; n--)
+            {
+                ct.ThrowIfCancellationRequested();
+
+                checked
+                {
+                    sum += n;
+                }
+            }
+
+            return sum;
+        }
+
+        public void DoTaskSum()
+        {
+            //Task<Int32> t = new Task<Int32>(n => Sum((Int32)n), 10);
+
+            //Task t2 = new Task(() => Console.WriteLine("Hello,World!"));
+
+            //t.Start();
+
+            //t.Wait();
+
+            //Console.WriteLine("The sum is:" + t.Result);
+
+            //CancellationTokenSource cts = new CancellationTokenSource();
+
+            //Task<Int32> t3 = new Task<Int32>(n => SumWithCancelToken(cts.Token, (Int32)n), 10000);
+            //t3.Start();
+            //Equal to the following
+
+            //Task<Int32> t3 = Task.Run(() => SumWithCancelToken(cts.Token, 10000), cts.Token);
+
+            //Task cwt = t3.ContinueWith(task => Console.WriteLine("Hello,The sum is:" + task.Result));
+
+            //cts.Cancel();
+
+            //try
+            //{
+            //    Console.WriteLine("The sum is:", t3.Result);
+            //    Console.WriteLine();
+            //}
+            //catch (AggregateException ex)
+            //{
+            //    ex.Handle(e => e is OperationCanceledException);
+
+            //    Console.WriteLine("Sum was canceled");
+            //}
+
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Task<Int32> t4 = Task.Run(() => Sum(10000));
+
+            t4.ContinueWith(task => Console.WriteLine("Hello, this is continue with function= {0}", task.Result), TaskContinuationOptions.ExecuteSynchronously);
+            t4.ContinueWith(task => Console.WriteLine("Sum was canceled"), TaskContinuationOptions.OnlyOnCanceled);
+
+            Console.WriteLine(t4.Result);
         }
     }
 }
