8c1176a295edb6ce6bcaab3105a5b2a4b2aaf3ce
YinXi <yinximail@gmail.com>
2019-05-16 20:07:47 +0800

    Add Interface demo

---
+++ CSharpFundamental/Interface/IAnimialBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamental.Interface
+{
+    interface IAnimialBehaviour
+    {
+        Int32 GetFurNum();
+    }
+}
---
+++ CSharpFundamental/Interface/Retriever.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamental.Interface
+{
+    class Retriever : Dog
+    {
+        public override Int32 GetFurNum()
+        {
+            return 300;
+        }
+
+//        public override Int64 GetFurNumLong()
+//        {
+//            return 1000;
+//        }
+    }
+}
