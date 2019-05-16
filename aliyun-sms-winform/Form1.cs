92c743c9cd94c0df8d85304ac422d97e2e57ea2d
YinXi <yinximail@gmail.com>
2019-05-16 19:38:21 +0800

    Add winform demo

---
+++ aliyun-sms-winform/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Profile;
+
+namespace aliyun_sms_winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendMessageToMe();
+        }
+
+        private void SendMessageToMe()
+        {
+            var profile = DefaultProfile.GetProfile("cn-hangzhou", "", "");
+
+            var client = new DefaultAcsClient(profile);
+
+            CommonRequest request = new CommonRequest()
+            {
+                Method = MethodType.POST,
+                Domain = "dysmsapi.aliyuncs.com",
+                Version = "2017-05-25",
+                Action = "SendSms",
+            };
+
+            request.AddQueryParameters("PhoneNumbers", "XXX");
+            request.AddQueryParameters("SignName", "晚归提醒");
+            request.AddQueryParameters("TemplateCode", "XXX");
+            request.AddQueryParameters("TemplateParam", "{code:200}");
+
+            try
+            {
+                label1.Text = "Start get the sms response";
+                CommonResponse response = client.GetCommonResponse(request);
+
+                label1.Text = Encoding.UTF8.GetString(response.HttpResponse.Content);
+            }
+            catch (Exception e)
+            {
+                label1.Text = e.ToString();
+                throw;
+            }
+        }
+    }
+}
---
+++ aliyun-sms-winform/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("aliyun-sms-winform")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("aliyun-sms-winform")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("083b9998-373d-4784-a8fa-54df9e7c479c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
