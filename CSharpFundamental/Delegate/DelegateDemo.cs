23a38785de776271893711c2990db7cb2f656802
YinXi <yinximail@gmail.com>
2019-05-17 18:21:59 +0800

    Add delegate demo

---
+++ CSharpFundamental/Delegate/DelegateDemo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamental.Delegate
+{
+    class DelegateDemo
+    {
+        internal delegate void Feedback(Int32 value);
+    }
+}
