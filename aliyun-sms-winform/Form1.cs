657aea9cb1a93bfb7d63d5e675c67df2b2937007
YinXi <yinximail@gmail.com>
2019-05-22 09:59:17 +0800

    Add aws ec2 demo

--- aliyun-sms-winform/Form1.cs
+++ aliyun-sms-winform/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
 using Aliyun.Acs.Core;
 using Aliyun.Acs.Core.Http;
 using Aliyun.Acs.Core.Profile;
---
+++ aws-ec2-demo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+
+namespace aws_ec2_demo
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            InitializeProfile();
+
+            var chain = new CredentialProfileStoreChain();
+
+            if (chain.TryGetAWSCredentials("basic_profile", out AWSCredentials credential))
+            {
+                var ec2Client = new AmazonEC2Client(credential, RegionEndpoint.APSoutheast2);
+
+                var response = await ec2Client.DescribeAccountAttributesAsync();
+
+                foreach (var item in response.AccountAttributes)
+                {
+                    Console.WriteLine(item.AttributeName + "|" + item.AttributeValues.Count);
+                }
+            }
+        }
+
+        static void InitializeProfile()
+        {
+            var options = new CredentialProfileOptions
+            {
+                AccessKey = "33",
+                SecretKey = "444+Puwxq+"
+            };
+
+            var profile = new CredentialProfile("basic_profile", options)
+            {
+                Region = RegionEndpoint.APSoutheast2
+            };
+
+            var netSDKFile = new NetSDKCredentialsFile();
+            netSDKFile.RegisterProfile(profile);
+        }
+    }
+}
