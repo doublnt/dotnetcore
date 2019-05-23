ac43af7b2db80ce0e9da2cdf7cd57ede6705405f
YinXi <yinximail@gmail.com>
2019-05-23 14:04:03 +0800

    Add bitmap

--- CSharpFundamental/MultipleThreadCompete.cs
+++ CSharpFundamental/MultipleThreadCompete.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,20 +15,24 @@ namespace CSharpFundamental
         List<int> numList = new List<int>();
         Random random = new Random();
         CancellationTokenSource cts = new CancellationTokenSource();
-        private const int ENDNUM = 100000;
+        private const int TOTAL_NUM = 10000000;
+        private const int CURRENT_THREAD_COUNT = 10;
 
         ReaderWriterLockSlim rwls = new ReaderWriterLockSlim();
 
+        int[] bitMap = new int[TOTAL_NUM];
+
         internal void DoTheCompeteSecond()
         {
+            //ThreadPool.SetMinThreads(CURRENT_THREAD_COUNT, CURRENT_THREAD_COUNT);
             Stopwatch sw = new Stopwatch();
             sw.Start();
-            Task[] tasks = new Task[100];
+            Task[] tasks = new Task[CURRENT_THREAD_COUNT];
 
-            for (int i = 0; i < 100; ++i)
+            for (int i = 0; i < CURRENT_THREAD_COUNT; ++i)
             {
                 int num = i;
-                tasks[i] = Task.Run(() => AddNumToList(num, cts), cts.Token);
+                tasks[i] = Task.Run(() => ExecuteTheTask(num, cts), cts.Token);
             }
 
             Task.WaitAny(tasks);
@@ -40,17 +47,15 @@ namespace CSharpFundamental
 
         private void AddToList(int num)
         {
-            rwls.EnterReadLock();
-            if (numList.Count == ENDNUM)
+            if (numList.Count == TOTAL_NUM)
                 return;
-            rwls.ExitReadLock();
-
-            rwls.EnterWriteLock();
             numList.Add(num);
-            rwls.ExitWriteLock();
+
+            var index = num % TOTAL_NUM;
+            bitMap[index] = 1;
         }
 
-        private void AddNumToList(object state, CancellationTokenSource cts)
+        private void ExecuteTheTask(object state, CancellationTokenSource cts)
         {
             //Console.WriteLine("This is the {0} thread,Current ThreadId={1}",
             //    state,
@@ -61,7 +66,7 @@ namespace CSharpFundamental
                 try
                 {
                     rwls.EnterReadLock();
-                    if (numList.Count == ENDNUM)
+                    if (numList.Count == TOTAL_NUM)
                     {
                         cts.Cancel();
                         Console.WriteLine("Current Thread Id={0},Current Count={1}",
@@ -76,18 +81,30 @@ namespace CSharpFundamental
                 }
 
                 var insertNum = GenerateInt32Num();
-                if (numList.Contains(insertNum))
+
+                rwls.EnterReadLock();
+                if (NumContains(insertNum))
                 {
                     insertNum = GenerateInt32Num();
                 }
+                rwls.ExitReadLock();
 
+                rwls.EnterWriteLock();
                 AddToList(insertNum);
+                rwls.ExitWriteLock();
             }
         }
 
         private int GenerateInt32Num()
         {
-            return random.Next(1, ENDNUM);
+            var num = random.Next(0, TOTAL_NUM);
+            //Console.WriteLine(num);
+            return num;
+        }
+
+        private bool NumContains(int num)
+        {
+            return bitMap[num] == 1;
         }
     }
 }
