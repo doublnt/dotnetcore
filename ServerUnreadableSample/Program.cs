e9d8f80265b04fa20cc3c3f369e57cc3623da6d1
YinXi <yinximail@gmail.com>
2019-06-05 20:12:10 +0800

    Add Pose demo support

---
+++ ServerUnreadableSample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+using Pose;
+
+namespace ServerUnreadableSample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            HttpWebRequest httpWebRequest = null;
+
+            var url = "https://portal.sfdsafahadowsocks.com/";
+
+            try
+            {
+                //httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
+                //httpWebRequest.Timeout = 3000;
+
+                //var response = httpWebRequest.GetResponse() as HttpWebResponse;
+
+                //Console.WriteLine(response.ResponseUri);
+
+                //if (response.StatusCode == HttpStatusCode.ServiceUnavailable)
+                //{
+                //}
+
+                Shim exceptionResponse = Shim.Replace(() => WebRequest.Create(url).GetResponse())
+                    .With(() => { new SocketException(); });
+            }
+            catch (SocketException socketException)
+            {
+                Console.WriteLine("SocketException" + socketException.SocketErrorCode);
+
+                throw;
+            }
+            catch (WebException webException)
+            {
+                Console.WriteLine(webException.Status + "Request Url is: " + httpWebRequest.RequestUri);
+
+                throw;
+            }
+        }
+    }
+}
