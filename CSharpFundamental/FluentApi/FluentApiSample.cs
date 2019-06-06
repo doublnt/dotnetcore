5385b022c614d09521b1e29adde5b5cc7e975569
YinXi <yinximail@gmail.com>
2019-06-06 18:58:01 +0800

    Add fluent api demo

---
+++ CSharpFundamental/FluentApi/FluentApiSample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamental.FluentApi
+{
+    public static class FluentApiSample
+    {
+        public static string Append(this IMarket market)
+        {
+            var appendValue = "AppendYourself";
+
+            Console.WriteLine(market + appendValue);
+
+            return appendValue;
+        }
+    }
+
+    public interface IMarket
+    {
+        string GetValue(string args);
+    }
+}
