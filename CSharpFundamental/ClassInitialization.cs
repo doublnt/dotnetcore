c6e5d25bbf40b244fad3936beae81de7d7f226d2
YinXi <yinximail@gmail.com>
2019-06-13 16:36:56 +0800

    Class test

--- CSharpFundamental/ClassInitialization.cs
+++ CSharpFundamental/ClassInitialization.cs
@@ -9,6 +9,8 @@ namespace CSharpFundamental
     static class ClassInitialization
     {
         public static bool ThrowException { get; set; } = true;
+
+        private static System.String valueString = "10086";
     }
 
     class FailingClass
