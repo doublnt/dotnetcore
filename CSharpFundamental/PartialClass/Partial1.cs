9fafaace9bdcfb19d33668a1a6afa2aa86e795a8
YinXi <yinximail@gmail.com>
2019-06-19 18:24:38 +0800

    Add partial class

--- CSharpFundamental/PartialClass/Partial1.cs
+++ CSharpFundamental/PartialClass/Partial1.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.SessionState;
 
 namespace CSharpFundamental.PartialClass
 {
     internal partial class Partial
     {
         public string Name { get; set; } = "Robert";
+
+        static Partial()
+        {
+            Console.WriteLine("Static Constructor!");
+        }
 
         public void RunThePartial1()
         {
