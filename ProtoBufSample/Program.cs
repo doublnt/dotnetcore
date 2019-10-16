2f499c4a408d2005fd35efc5730f7a6ad3b614b1
YinXi <yinximail@gmail.com>
2019-10-16 10:27:36 +0800

    Update Protobuf demo

--- ProtoBufSample/Program.cs
+++ ProtoBufSample/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.IO;
+using ProtoBuf;
+using ProtoBufSample.Proto;
 
 namespace ProtoBufSample
 {
     class Program
     {
+        private static string s_path = "D://Working//dotnetcore//ProtoBufSample//person.bin";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("---------------------");
+
+
+            var person = new Person()
+            {
+                Id = 10086,
+                Name = "Robert",
+                Address = new Address() { Line1 = "ZhongGuo", Line2 = "Zhejiang" }
+            };
+
+            using (var file = File.Create(s_path))
+            {
+                Serializer.Serialize(file, person);
+            }
+
+
+            using (var file2 = File.OpenRead(s_path))
+            {
+                var newPerson = Serializer.Deserialize<Person>(file2);
+
+                Console.WriteLine(newPerson.Address.Line2 + "\n" + newPerson.Address.Line1 + "\n" + newPerson.Name + "\n" + newPerson.Id);
+            }
         }
     }
 }
---
+++ ProtoBufSample/Proto/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+using ProtoBuf;
+
+namespace ProtoBufSample.Proto
+{
+    [ProtoContract]
+    public class Person
+    {
+        [ProtoMember(1)]
+        public int Id { get; set; }
+
+        [ProtoMember(2)]
+        public string Name { get; set; }
+
+        [ProtoMember(3)]
+        public Address Address { get; set; }
+    }
+
+    [ProtoContract]
+    public class Address
+    {
+        [ProtoMember(1)]
+        public string Line1 { get; set; }
+
+        [ProtoMember(2)]
+        public string Line2 { get; set; }
+    }
+}
