5d121714e7c8123dfb5ed19d875019cf5657aceb
YinXi <yinximail@gmail.com>
2019-11-12 14:54:14 +0800

    Update code

--- ProtoBufSample/Program.cs
+++ ProtoBufSample/Program.cs
@@ -3,6 +3,8 @@ using System.IO;
 using ProtoBuf;
 using ProtoBufSample.Proto;
 
+using Console = System.Console;
+
 namespace ProtoBufSample
 {
     class Program
@@ -11,29 +13,36 @@ namespace ProtoBufSample
 
         static void Main(string[] args)
         {
+            double value = double.NaN;
+
             Console.WriteLine("Hello World!");
-            Console.WriteLine("---------------------");
+
+            Console.WriteLine($"double.NaN == double.NaN：{double.NaN == double.NaN}");
+
+            Console.WriteLine($"double.IsNaN：{double.IsNaN(value)}");
 
 
-            var person = new Person()
-            {
-                Id = 10086,
-                Name = "Robert",
-                Address = new Address() { Line1 = "ZhongGuo", Line2 = "Zhejiang" }
-            };
-
-            using (var file = File.Create(s_path))
-            {
-                Serializer.Serialize(file, person);
-            }
 
 
-            using (var file2 = File.OpenRead(s_path))
-            {
-                var newPerson = Serializer.Deserialize<Person>(file2);
+            //var person = new Person()
+            //{
+            //    Id = 10086,
+            //    Name = "Robert",
+            //    Address = new Address() { Line1 = "ZhongGuo", Line2 = "Zhejiang" }
+            //};
 
-                Console.WriteLine(newPerson.Address.Line2 + "\n" + newPerson.Address.Line1 + "\n" + newPerson.Name + "\n" + newPerson.Id);
-            }
+            //using (var file = File.Create(s_path))
+            //{
+            //    Serializer.Serialize(file, person);
+            //}
+
+
+            //using (var file2 = File.OpenRead(s_path))
+            //{
+            //    var newPerson = Serializer.Deserialize<Person>(file2);
+
+            //    Console.WriteLine(newPerson.Address.Line2 + "\n" + newPerson.Address.Line1 + "\n" + newPerson.Name + "\n" + newPerson.Id);
+            //}
         }
     }
 }
