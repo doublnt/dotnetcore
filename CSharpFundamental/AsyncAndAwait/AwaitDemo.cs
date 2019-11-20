969b7b657a808dcae1947d1c74a93094013c71b7
YinXi <yinximail@gmail.com>
2019-11-20 14:02:26 +0800

    IL Code analyse

---
+++ CSharpFundamental/AsyncAndAwait/AwaitDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CSharpFundamental.AsyncAndAwait
+{
+    public class AwaitDemo
+    {
+        private static readonly HttpClient HttpClient = new HttpClient();
+        private readonly Uri Uri = new Uri("http://www.baidu.com");
+
+        public async Task<string> GetResponse()
+        {
+            var response = await HttpClient.GetAsync(Uri);
+
+            return response.Version.ToString();
+        }
+    }
+}
