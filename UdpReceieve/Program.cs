e6c60a4836044ce63069b13a92476a12b8027d05
YinXi <yinximail@gmail.com>
2019-12-05 19:19:45 +0800

    Udp Send And Receive Demo

---
+++ UdpReceieve/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpReceive
+{
+    class Program
+    {
+        private static readonly int _port = 11000;
+
+        static void Main(string[] args)
+        {
+            var udpListener = new UdpClient(_port);
+            var ipEndpoint = new IPEndPoint(IPAddress.Any, _port);
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+
+                    var bytes = udpListener.Receive(ref ipEndpoint);
+
+                    Console.WriteLine($"Receive broadcast from {ipEndpoint}");
+
+                    Console.WriteLine($"Encoding {Encoding.ASCII.GetString(bytes)}");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (Exception e)
+            {
+                udpListener.Close();
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UdpSend/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Send();
+        }
+
+        private static void Send()
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            var boardCast = IPAddress.Parse("127.0.0.1");
+
+
+            var buffer = Encoding.ASCII.GetBytes("Udp Send Test...");
+
+            var ipEndpoint = new IPEndPoint(boardCast, 11000);
+
+            socket.SendTo(buffer, ipEndpoint);
+
+            Console.WriteLine("Message have sent to the broadcast.");
+        }
+    }
+}
