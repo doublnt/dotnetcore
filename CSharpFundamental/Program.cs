81fff75387f888bea8a0e712f3d5bb8fb6503166
Yin Xi <yinximail@gmail.com>
2020-01-22 19:17:25 +0800

    Update Code

--- CSharpFundamental/Program.cs
+++ CSharpFundamental/Program.cs
@@ -1,24 +1,28 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
-
-using CSharpFundamental.AsyncAndAwait;
-using CSharpFundamental.Dictionary;
-using CSharpFundamental.Enum;
-using CSharpFundamental.InteropServiceCom;
-using CSharpFundamental.StringMethod;
-using CSharpFundamental.ThreadSafe;
 
 namespace CSharpFundamental
 {
     internal class Program
     {
-        private static void Main(string[] args)
-        {
-            new StopWatch().Run();
+        private static Func<string> TestFunc;
 
-            Console.ReadLine();
+        public static void Main(string[] args)
+        {
+            InvokeDelegate();
+            Console.WriteLine(TestFunc());
+        }
+
+        private static void InvokeDelegate()
+        {
+            TestFunc += GetResult;
+            // Equal to below
+            
+            
+        }
+
+        public static string GetResult()
+        {
+            return "TESTTEST";
         }
     }
 }
---
+++ CSharpFundamental/TaskTest/TaskDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpFundamental.TaskTest
+{
+    public class TaskDemo
+    {
+        public async void Run()
+        {
+            var task = Enumerable.Range(0, 10).Select(i => Task.Run(() => LongTimeTask(i))).ToList();
+
+            await Task.WhenAll(task);
+
+            Console.ReadLine();
+        }
+
+        private static void LongTimeTask(int i)
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId.ToString().PadLeft(2, ' ');
+            var line = i.ToString().PadLeft(2, ' ');
+
+            //Console.WriteLine($"[{line}] [{threadId}] [{TaskScheduler.Default}]");
+            Console.WriteLine($"[{line}] [{threadId}] [{DateTime.Now:yyyyMMddHHmmss.fff}] 异步任务已开始。。。");
+
+            Thread.Sleep(5000);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"[{line}] [{threadId}] [{DateTime.Now:yyyyMMddHHmmss.fff}] 异步任务已结束。。。");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
