2202b5902c4a4c7e01a144c75b36adec16a427fb
Xi Yin <yinximail@gmail.com>
2020-09-02 10:53:22 +0800

    Async await wpf demo

--- WPF/CodeOnlyWpf/Windows/StackPanelWindow.cs
+++ WPF/CodeOnlyWpf/Windows/StackPanelWindow.cs
@@ -1,10 +1,12 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace CodeOnlyWpf.Windows
 {
@@ -23,19 +25,25 @@ namespace CodeOnlyWpf.Windows
 
             var scrollView = new ScrollViewer();
             var border = new Border();
+            var wrapPanel = new StackPanel();
 
-            var wrapPanel = new WrapPanel();
-
-            var button = new Button() { Content = "Test" };
+            var button = new Button()
+            {
+                Content = "Click1"
+            };
             wrapPanel.Children.Add(button);
-            _textBlock.Text = "111111111";
+
+            var button2 = new Button
+            {
+                Content = "Click2"
+            };
+            wrapPanel.Children.Add(button2);
 
             wrapPanel.Children.Add(_textBlock);
 
             button.Click += Button_Click;
             border.Child = wrapPanel;
             scrollView.Content = border;
-
             Content = scrollView;
         }
 
@@ -43,7 +51,20 @@ namespace CodeOnlyWpf.Windows
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            //GetContent().Wait();
+
+            //var a = GetContent().Result;
+
+            _textBlock.Text = Thread.CurrentThread.ManagedThreadId.ToString();
+
             await GetContent();
+
+            _textBlock.Text = "111";
+
+            SynchronizationContext.Current.Post(_ => { }, null);
+
+            Task.Delay(3000)
+                .Run(() => { _textBlock.Text = "222"; });
         }
 
         private async Task<string> GetContent()
@@ -51,8 +72,36 @@ namespace CodeOnlyWpf.Windows
             using (var httpClient = new HttpClient())
             using (var httpResponse = await httpClient.GetAsync("http://www.baidu.com"))
             {
-                return await httpResponse.Content.ReadAsStringAsync();
+                var result = await httpResponse.Content.ReadAsStringAsync();
+                _textBlock.Text = Thread.CurrentThread.ManagedThreadId.ToString();
+                return result;
             }
+        }
+
+        private Task<bool> GetContentTaskCompletionSourceVersion(Func<bool> func)
+        {
+            if (null == func)
+            {
+                throw new ArgumentNullException("action");
+            }
+
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            ThreadPool.QueueUserWorkItem(_ =>
+             {
+                 try
+                 {
+                     var result = func();
+
+                     taskCompletionSource.SetResult(result);
+                 }
+                 catch (Exception ex)
+                 {
+                     taskCompletionSource.SetException(ex);
+                 }
+             });
+
+            return taskCompletionSource.Task;
         }
     }
 }
