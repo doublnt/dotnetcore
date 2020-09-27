9d35279e012ef89bf1f8bfdfb73cbba680a9a48d
YinXi <yinximail@gmail.com>
2020-09-27 22:13:14 +0800

    Add t1114 solve code

---
+++ Algorithm/SortBinarySample/t1114.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Foo
+{
+    public Foo()
+    {
+
+    }
+
+    private bool _isFirstEnd = false;
+    private bool _isSecondEnd = false;
+
+    public void First(Action printFirst)
+    {
+
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        _isFirstEnd = true;
+    }
+
+    public void Second(Action printSecond)
+    {
+
+        // printSecond() outputs "second". Do not change or remove this line.
+
+        while (!_isFirstEnd)
+        {
+
+        }
+        printSecond();
+        _isSecondEnd = true;
+    }
+
+    public void Third(Action printThird)
+    {
+
+        // printThird() outputs "third". Do not change or remove this line.
+        while (!_isSecondEnd)
+        {
+
+        }
+
+        printThird();
+    }
+}
