9c92d27775a0d1ee6c41556c208261b6693f4e2c
YinXi <yinximail@gmail.com>
2021-03-07 11:22:49 +0800

    Databinding template

--- wpf-template/App.xaml.cs
+++ wpf-template/App.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+
+using wpf_template.Model;
 
 namespace wpf_template
 {
@@ -13,5 +16,20 @@ namespace wpf_template
     /// </summary>
     public partial class App : Application
     {
+        public ObservableCollection<Photo> Photos { get; set; } = new ObservableCollection<Photo>();
+        private void AppStartup(object sender, StartupEventArgs e)
+        {
+            Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("pack://application:,,,/Resources/ControlResources.xaml") });
+            LoadPhotoData();
+        }
+
+        private void LoadPhotoData()
+        {
+            var ph1 = new Photo("羿", "D://Location", true);
+            var ph2 = new Photo("上", "C://Location", true);
+
+            Photos.Add(ph1);
+            Photos.Add(ph2);
+        }
     }
 }
--- wpf-template/MainWindow.xaml.cs
+++ wpf-template/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
+using wpf_template.Model;
 
 namespace wpf_template
 {
