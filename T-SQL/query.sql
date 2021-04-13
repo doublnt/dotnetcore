5724522964062c9e9c607ed38c8a6820c21f6567
YinXi <yinximail@gmail.com>
2021-04-13 09:40:39 +0800

    Code sort.

--- T-SQL/query.sql
+++
@@ -1,8 +0,0 @@
-select * from dbo.orders;
-
-select empid, orderid, qty, year(orderts) as orderyear, count(*) as numorders
-from dbo.orders
-where orderid = 76
-group by empid, year(orderts)
-having count(*) > 1
-order by empid, orderyear;
